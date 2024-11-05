47e3c1b1ca5d519c37bb9afcf35f6b86bb8527e3
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-11-05 12:04:45 -0300

    feat: add setup initial

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const SubscriptionPage = () => {
+   return <div>subscription</div>;
+};
+
+export default SubscriptionPage;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const TransactionsPage = () => {
+   return <div>Transctions</div>;
+};
+
+export default TransactionsPage;
