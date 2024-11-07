866d7900416aec27049af8c79867082ea3ff1323
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-11-07 11:59:13 -0300

    feat: create add transaction dialog

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -6,28 +6,10 @@ import TransactionTypeBadge from "../_components/type-badge";
 import { formatCurrency } from "@/app/_helpers/currency";
 import { Button } from "@/app/_components/ui/button";
 import { PencilIcon, TrashIcon } from "lucide-react";
-
-export const TRANSACTION_CATEGORY_LABELS = {
-   EDUCATION: "Educação",
-   ENTERTAINMENT: "Entretenimento",
-   FOOD: "Alimentação",
-   HEALTH: "Saúde",
-   HOUSING: "Moradia",
-   OTHER: "Outros",
-   SALARY: "Salário",
-   TRANSPORTATION: "Transporte",
-   UTILITY: "Utilidades",
-};
-
-export const TRANSACTION_PAYMENT_METHOD_LABELS = {
-   BANK_TRANSFER: "Transferência bancária",
-   BANK_SLIP: "Boleto bancário",
-   CASH: "Dinheiro",
-   CREEDIT_CARD: "Cartão de crédito",
-   DEBIT_CARD: "Cartão de débito",
-   PIX: "PIX",
-   OTHER: "Outros",
-};
+import {
+   TRANSACTION_CATEGORY_LABELS,
+   TRANSACTION_PAYMENT_METHOD_LABELS,
+} from "@/app/_constants/transactions";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
    {
