6e1d97c7400410d243099d81af7ad0612c2c36f1
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-11-08 20:38:42 -0300

    feat: ensure pages aren correctly scrolled by the user

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -9,8 +9,10 @@ interface IPercentageItemProps {
 const PercentageItem = ({ icon, type, percentage }: IPercentageItemProps) => {
    return (
       <div className="flex items-center justify-between">
-         <div className="flex items-center gap-2">
-            {icon}
+         <div className="flex items-center gap-3">
+            <div className="rounded-lg bg-white bg-opacity-[3%] p-2">
+               {icon}
+            </div>
             <p className="text-sm text-muted-foreground">{type}</p>
          </div>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
                   baseTheme: dark,
                }}
             >
-               {children}
+               <div className="flex h-full flex-col overflow-hidden">
+                  {children}
+               </div>
             </ClerkProvider>
          </body>
       </html>
