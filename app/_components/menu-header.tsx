ef2996ae873ec7e32ca3024957e6f5568c34f5e1
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-12-23 14:52:31 -0300

    feat: add menu header component

--- app/_components/menu-header.tsx
+++ app/_components/menu-header.tsx
@@ -1,18 +1,72 @@
+"use client";
+
 import { MenuIcon } from "lucide-react";
-import { Button } from "./ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import {
+   Sheet,
+   SheetContent,
+   SheetDescription,
+   SheetHeader,
+   SheetTrigger,
+} from "./ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const MenuHeader = () => {
+   const pathname = usePathname();
+
    return (
       <nav className="flex lg:hidden">
          <Sheet>
             <SheetTrigger asChild>
-               <Button variant="ghost">
-                  <MenuIcon size={10} />
-               </Button>
+               <MenuIcon className="size-5" />
             </SheetTrigger>
 
-            <SheetContent>teste</SheetContent>
+            <SheetContent>
+               <SheetHeader>
+                  <Image
+                     src="/logo.svg"
+                     alt="Finance Ai"
+                     width={123}
+                     height={39}
+                  />
+               </SheetHeader>
+
+               <SheetDescription>
+                  <div className="mt-8 flex flex-col gap-4">
+                     <Link
+                        href="/"
+                        className={`w-full rounded-sm px-3 py-1 text-base font-bold hover:bg-gray-700/20 hover:text-primary ${
+                           pathname === "/"
+                              ? "bg-gray-700/20 font-bold text-primary"
+                              : "text-muted-foreground"
+                        }`}
+                     >
+                        Dashboard
+                     </Link>
+                     <Link
+                        href="/transactions"
+                        className={`w-full rounded-sm px-3 py-1 text-base font-bold hover:bg-gray-700/20 hover:text-primary ${
+                           pathname === "/transactions"
+                              ? "bg-gray-700/20 font-bold text-primary"
+                              : "text-muted-foreground"
+                        }`}
+                     >
+                        Transações
+                     </Link>
+                     <Link
+                        href="/subscription"
+                        className={`w-full rounded-sm px-3 py-1 text-base font-bold hover:bg-gray-700/20 hover:text-primary ${
+                           pathname === "/subscription"
+                              ? "bg-gray-700/20 font-bold text-primary"
+                              : "text-muted-foreground"
+                        }`}
+                     >
+                        Assinaturas
+                     </Link>
+                  </div>
+               </SheetDescription>
+            </SheetContent>
          </Sheet>
       </nav>
    );
